9209115cbad352f4e61083ca93d955fc6c6f1665
Majd833 <lina.azzam.1611@gmail.com>
2024-12-12 17:14:28 +0200

    HW

--- Flashcards.py
+++ Flashcards.py
@@ -9,7 +9,7 @@ while (True):
     word=input("enter a word to add to your flashcards:")
     meaning=input("Add the definition of the word:")
     flash.append(flashcards(word,meaning))
-    option=input("If you want to add some more words to your flashcards press 0 if not press1:")
+    option=input("If you want to add some more words to your flashcards press 0 if not press 1:")
     if (option):
         break
 for i in flash:
