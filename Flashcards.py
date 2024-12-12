acfcb70eac4d11dab5d9e6108924ab40353f5a34
Majd833 <lina.azzam.1611@gmail.com>
2024-12-12 16:21:30 +0200

    activity 2

---
+++ Flashcards.py
@@ -0,0 +1,16 @@
+class flashcards:
+    def __init__(self,word,meaning):
+        self.word=word
+        self.meaning=meaning
+    def __str__(self):
+        return self.word+'('+self.meaning+')'
+flash=[]
+while (True):
+    word=input("enter a word to add to your flashcards:")
+    meaning=input("Add the definition of the word:")
+    flash.append(flashcards(word,meaning))
+    option=input("If you want to add some more words to your flashcards press 0 if not press1:")
+    if (option):
+        break
+for i in flash:
+    print(">",i)
